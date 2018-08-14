737b5fd8fc133dfa01151d32bb6e9cd1f463a3cf
dorothy-ng <42361183+dorothy-ng@users.noreply.github.com>
2018-08-13 20:55:20 -0400

    I learned math!

---
+++ dorothy_likes_coding.py
@@ -0,0 +1,16 @@
+def age(a):
+    if 0 <= a <= 10:
+        print("you're too young")
+    if 11 <= a <= 20:
+        print("you are just right... for marcus")
+        
+    if 21 <= a:
+        print("you are too old for marcus")
+    if -10 <= a <= -1:
+        print ("you should go back to the womb")
+    if a <= -1:
+        print ("doro is sad")
+        
+        
+
+    
